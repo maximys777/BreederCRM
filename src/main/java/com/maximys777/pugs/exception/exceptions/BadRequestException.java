5e2596a2854385aab671d82b5869e9a44294c986
Maksym <48604958+maximys777@users.noreply.github.com>
2025-12-08 10:53:42 +0200

    Update getFileExtension method in S3Service. Add new exception, develop new tests for delete method

---
+++ src/main/java/com/maximys777/pugs/exception/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.maximys777.pugs.exception.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
