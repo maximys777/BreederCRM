539264174fd30336310277949d0dff584324b39a
Maks <maxx4449@gmail.com>
2026-01-08 16:38:30 +0200

    Update text, change class -> record

--- src/main/java/com/maximys777/pugs/feedback/service/FeedbackCreatedEvent.java
+++ src/main/java/com/maximys777/pugs/feedback/service/FeedbackCreatedEvent.java
@@ -1,11 +1,6 @@
 package com.maximys777.pugs.feedback.service;
 
 import com.maximys777.pugs.feedback.entity.FeedbackEntity;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
-public class FeedbackCreatedEvent {
-    private final FeedbackEntity feedbackEntity;
+public record FeedbackCreatedEvent(FeedbackEntity feedbackEntity) {
 }
